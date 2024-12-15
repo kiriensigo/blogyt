70c7b83b8d6a8f418954fcd4769c8c860ffa191e
kiriensigo <kiriensigo@gmail.com>
2024-12-15 23:46:49 +0900

    const paths

--- blog_client/src/pages/api/posts/[id].tsx
+++ blog_client/src/pages/api/posts/[id].tsx
@@ -1,5 +1,24 @@
 import React from react";
 
+typre Props = {
+    post: Post;
+};
+
+export async function getStaticPaths() {
+    const resi = await fetch("http://localhost:3001/api/v1/posts")
+    const posts: Post[] =await resi.json();
+
+    const paths = posts.map((post) => ) ({
+        params: {id: posts.indexOf.toString()} ,
+    }));
+
+    return {
+        paths,
+        fallback: true,
+    };
+}
+
+
 export async function getStaticProps({params}): { params: { id: strring } }) {
     const res = await fetch("https://localhost:3000/api/v1/posts/${params.id}");
     const posts = await res.json();
