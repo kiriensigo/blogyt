e02af8e2f2a9466288025d53cfbce230ee681675
kiriensigo <kiriensigo@gmail.com>
2024-12-24 23:44:33 +0900

    const handlesubmit

--- blog_client/src/pages/create-post.tsx
+++ blog_client/src/pages/create-post.tsx
@@ -1,17 +1,25 @@
 import React from "react"
+import styles from "../styles/Home.module.css";
 
 const CreatePost = () => {
-    return (
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+
+    const handleSubmit = (e)
+    =>
+
+        return (
     <div className={Styles.container}>
         <h1 className={Styles.title}>ブログ新規登録</h1>
-        <from className={Styles.form>
+        <from className={Styles.form} onSubmit={handleSubmit}>
             <label className={Styles.label}>タイトル</label>
-            <input type="text" className={StyleSheet.input} />
-            </input>
+            <input
               type="text"
-              className={StyleSheet.input}
-              onChange= 
-              />
+              className={Styles.input}
+              onChange={(e: ChangeEvent<HTMLInputElement>)) =>
+                setTitle(e.target.value)
+              }
+            />
             <label>本文</label>
             <textarea />
             <button type="submit">投稿</button>
