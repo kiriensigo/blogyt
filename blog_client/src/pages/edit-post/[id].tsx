80a854b17a1aa285e31ad263c073aded31a1cd5b
kiriensigo <kiriensigo@gmail.com>
2025-01-06 23:41:23 +0900

    response

--- blog_client/src/pages/edit-post/[id].tsx
+++ blog_client/src/pages/edit-post/[id].tsx
@@ -4,9 +4,69 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import { Post } from "@/types";
 
+type Post = {
+    title: string;
+    content: string;
+    id: string;
+};
+
 type Props = {
     post: Post;
 };
+
+export default function EdiPost({ post }: Props) {
+    const [title, setTitle] = useState(post.title);
+    const [content, setContent] = useState(post.content);
+    const router = useRouter();
+
+    const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.target.value);
+    };
+    
+    const handleContentChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setContent(e.target.value);
+    };
+    
+    const handleSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.put(
+                'http://hocalhost:3001/api/v1/posts/${post.id}`,
+            {
+                title: title,
+                content: content,
+            }
+            );
+            if (response.status !== 200) {
+                throw new Error("投稿に失敗しました");
+            }
+            router.push("/");
+        } catch (err) {
+            alert("投稿に失敗しました");
+        }
+    };
+
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>ブログ編集</h1>
+            <form className={styles.form} onSubmit={handleSubmit}>
+                <label className={styles.label}>タイトル</label>
+                <input
+                    type="text"
+                    className={styles.input}
+                    value={title}
+                    onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                        setTitle(e.target.value)
+                    }
+                />
+                <label>本文</label>
+                <textarea />
+                <button type="submit">投稿</button>
+            </form>
+        </div>
+    );
+}
 
 export async function getServerSideProps(context: any) {
     const id = context.params.id;
