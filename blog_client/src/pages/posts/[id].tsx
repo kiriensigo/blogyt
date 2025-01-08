a23560d90ea18da410388befff3e61771326c199
kiriensigo <kiriensigo@gmail.com>
2025-01-09 00:22:03 +0900

    params id

--- blog_client/src/pages/posts/[id].tsx
+++ blog_client/src/pages/posts/[id].tsx
@@ -12,3 +12,25 @@ type Props = (
     post: Post;
 );
 
+export async function getStaticPaths() {
+    const res = await fetch("http://localhost:3001/api/v1/posts");
+    const posts: Post[] = await res.json();
+
+    const paths = posts.map((post: Post) => ({
+        params: { id: post.id.toString() },
+    }));
+
+    return { paths, fallback: true };
+}
+
+export async function getStaticPaths({ params }: { params:{ id:string } }) {
+    const res = await fetch("http://localhost:3001/api/v1/posts/${params.id}");
+    const posts: Post[] = await res.json();
+
+    return {
+        props: {
+            posts,
+        },
+        revalidate: 60,
+    };
+    }
