bf8ea1df8f8c9168f35e129639018c61fe6bcb6b
kiriensigo <kiriensigo@gmail.com>
2025-01-11 23:29:43 +0900

    createPost

--- blog_client/src/pages/create-post.tsx
+++ blog_client/src/pages/create-post.tsx
@@ -1,43 +1,57 @@
-import React, { FormEvent } from "react"
+import { ChangeEvent, FormEvent, useState } from "react";
 import styles from "../styles/Home.module.css";
+import axios from "axios";
+import { useRouter } from "next/router";
 
-const CreatePost = () => {
-    const [title, setTitle] = useState("");
-    const [content, setContent] = useState("");
+export default function CreatePost() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const router = useRouter();
 
-    const handleSubmit = (e: FormEvent) => {
-        e.preventDefalut();
+  const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
 
-    //APIを叩く
-    try{
-        await axios.post("http:localhost3001:/api/v1/posts");
+  const handleContentChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setContent(e.target.value);
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post("http://localhost:3001/api/v1/posts", {
         title: title,
         content: content,
-    });:
-
-    Router.push("/");
-    } catch (err) {
-        alert("投稿に失敗しました");
+      });
+      router.push("/");
+    } catch (error) {
+      alert("Error creating post");
+      console.error(error);
     }
-    };
+  };
 
-     return (
-    <div className={Styles.container}>
-        <h1 className={Styles.title}>ブログ新規登録</h1>
-        <from className={Styles.form} onSubmit={handleSubmit}>
-            <label className={Styles.label}>タイトル</label>
-            <input
-              type="text"
-              className={Styles.input}
-              onChange={(e: ChangeEvent<HTMLInputElement>)) =>
-                setTitle(e.target.value)
-              }
-            />
-            <label>本文</label>
-            <textarea />
-            <button type="submit">投稿</button>
-        </from>
-
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>ブログ新規登録</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <label className={styles.label}>Title:</label>
+        <input
+          className={styles.input}
+          type="text"
+          value={title}
+          onChange={handleTitleChange}
+        />
+        <label className={styles.label}>Content:</label>
+        <textarea
+          className={styles.textarea}
+          value={content}
+          onChange={handleContentChange}
+        />
+        <button className={styles.button} type="submit">
+          Submit
+        </button>
+      </form>
     </div>
-    );
-};
+  );
+}
