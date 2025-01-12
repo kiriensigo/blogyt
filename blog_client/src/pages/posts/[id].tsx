c671395b3341bd6be1a9f030d9d4823dab314d7a
kiriensigo <kiriensigo@gmail.com>
2025-01-12 20:58:08 +0900

    post index

--- blog_client/src/pages/posts/[id].tsx
+++ blog_client/src/pages/posts/[id].tsx
@@ -2,51 +2,55 @@ import { useRouter } from "next/router";
 import styles from "../../styles/Post.module.css";
 
 interface Post {
-    id:string;
-    title: string;
-    content: string;
-    created_at: string;
+  id: string;
+  title: string;
+  content: string;
+  created_at: string;
 }
 
-type Props = (
-    post: Post;
-);
+type Props = {
+  post: Post;
+};
 
 export async function getStaticPaths() {
-    const res = await fetch("http://localhost:3001/api/v1/posts");
-    const posts: Post[] = await res.json();
+  const res = await fetch("http://localhost:3001/api/v1/posts");
+  const posts: Post[] = await res.json();
 
-    const paths = posts.map((post: Post) => ({
-        params: { id: post.id.toString() },
-    }));
+  // Get the paths we want to pre-render based on posts
+  const paths = posts.map((post) => ({
+    params: { id: post.id.toString() },
+  }));
 
-    return { paths, fallback: true };
+  // We'll pre-render only these paths at build time.
+  // { fallback: 'blocking' } will server-render pages
+  // on-demand if not generated at build time.
+  return { paths, fallback: true };
 }
 
-export async function getStaticPaths({ params }: { params:{ id:string } }) {
-    const res = await fetch("http://localhost:3001/api/v1/posts/${params.id}");
-    const posts: Post[] = await res.json();
+export async function getStaticProps({ params }: { params: { id: string } }) {
+  const res = await fetch(`http://localhost:3001/api/v1/posts/${params.id}`);
+  const post = await res.json();
 
-    return {
-        props: {
-            posts,
-        },
-        revalidate: 60,
-    };
-    }
+  return {
+    props: {
+      post,
+    },
+    revalidate: 60, // In seconds
+  };
+}
 
-    export default function Post({ post }: Props) {
-        const router = useRouter();
+export default function Post({ post }: Props) {
+  const router = useRouter();
 
-        if (router.isFallback) {
-            return <div>Loading...</div>;
-        }
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
 
-        return (
-            <div className={styles.container}>
-                <h1 className={styles.title}>{post.title}</h1>
-                <div className={styles.date}>Posted on {post.created_at}</div>
-                <p className={styles.content}>{post.content}</p>
-            </div>
-        );
-    }
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>{post.title}</h1>
+      <div className={styles.date}>Posted on {post.created_at}</div>
+      <p className={styles.content}>{post.content}</p>
+    </div>
+  );
+}
